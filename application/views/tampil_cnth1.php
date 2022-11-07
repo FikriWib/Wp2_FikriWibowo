b3d37a4792642097198bf4e5a00d9c4955dc99a0
FikriWib <19210825@gmail.com>
2022-11-07 10:18:44 +0700

    Pustaka booking

---
+++ application/views/tampil_cnth1.php
@@ -0,0 +1,4 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+echo "<h1>Perkenalkan</h1>";
+echo "Nama saya Imam Nawawi Saya tingga di daerah Ciputat olah raga yang saya sukai adalah Bulutangkis";
